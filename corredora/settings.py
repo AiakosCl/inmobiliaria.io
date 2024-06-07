19192936a3b14955975a2c8bb7158b496e9e7ca1
Gerardo Alvarez <gerardo.alvarez@outlook.com>
2024-06-06 20:09:22 -0400

    actualizacion 2

--- corredora/settings.py
+++ corredora/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': '127.0.0.1',
+        'HOST': os.environ.get('HOST_NAME'),
         'PORT': '5432',
     }
 }
